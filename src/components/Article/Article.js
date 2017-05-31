524a74bb3436616effa4002e2e3757a6cadd25ed
LDQ-first <178848890@qq.com>
2017-05-31 22:47:37 +0800

    fixing a bug

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -58,11 +58,13 @@ export default {
              .then( () => this.$store.commit('showLoading', false))
              .then( () => {
                  const articleContent = this.$refs.articleContent;
-                 const articleReply = this.$refs.articleReply;
-                 this.articleImgs = articleContent.querySelectorAll('img');
-                 this.articleReplyImgs = articleReply.querySelectorAll('img');
+                 let articleReply = this.$refs.articleReply;
+                 this.articleImgs = articleContent.querySelectorAll('img:not(.avatar-img)');
+                 this.articleReplyImgs = articleReply.querySelectorAll('img:not(.avatar-img)');
                  this.showModal(this.articleImgs);
                  this.showModal(this.articleReplyImgs);
+                 //  console.log(this.articleReplyImgs);
+                 
              })
     },
     computed: {
@@ -78,11 +80,17 @@ export default {
     },
     methods: {
         showModal(imgs) {
+            console.log(imgs);
+            imgs.forEach((img, idx) => {
+                console.log(img);
+            })
             for(let img of imgs) {
+              //  console.log(img);
              img.onclick = () => { 
                 if(img.parentNode.tagName === 'A') {
                     return;
                 }
+              /*  console.log(img);*/
                 this.modalImgSrc = img.src;
              }
             }
@@ -90,10 +98,12 @@ export default {
         sort( e, type) {
             if(this.replies && this.replies.length > 0) {
                const target = e.target;
-               if(target.classList.contains('active')) return;
+               if(target.classList.contains('active')) {
+                   return;
+                }
                this.replies.sort((a, b) => {
                    this.sortWay = type;
-
+                 //  console.log(this.articleReplyImgs);
                    if(type === 'normal' ) {
                        return new Date(b.create_at) - new Date(a.create_at);
                    }
@@ -103,11 +113,13 @@ export default {
                    else if(type === 'ups') {
                        return b.ups.length - a.ups.length;
                    }
-
-                   
-
+                  
                })
-
+                const articleReply = this.$refs.articleReply;
+                this.articleReplyImgs = articleReply.getElementsByTagName('img');
+                //console.log( this.articleReplyImgs);
+                this.showModal( this.articleReplyImgs);
+             
             }
         },
         collect() {
@@ -118,6 +130,25 @@ export default {
         },
         ups() {
 
+        }
+    },
+    watch: {
+        replies() {
+             let articleReply = this.$refs.articleReply;
+            // console.log(articleReply);
+           //  this.articleReplyImgs = articleReply.getElementsByTagName('img');
+          //   this.articleReplyImgs = articleReply.querySelectorAll('img:not(.avatar-img)');
+            // console.log(this.articleReplyImgs);
+             if(this.articleReplyImgs && this.articleReplyImgs.length) {
+             //  this.showModal(this.articleReplyImgs);
+               // console.log(this.articleReplyImgs);
+             }
+          
+             
+             /*this.articleReplyImgs = articleReply.querySelectorAll('img:not(.avatar-img)');*/
+             
+            /* this.showModal(this.articleReplyImgs);*/
+            /*  console.log(this.articleReplyImgs);*/
         }
     },
     components: {
