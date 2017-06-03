3f7ed70181d7a95e8bea270a256d82b3612fb244
LDQ-first <178848890@qq.com>
2017-06-03 16:41:56 +0800

    add login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,27 @@
-
+import axios from 'axios'
+import Buttons from '../button/button.vue'
 
 export default {
     name: 'Login',
     data() {
         return {
-            
+            AccessToken: ''
         }
     },
+     computed: {
+        skinColor() {
+            return this.$store.state.skinColor;
+        },
+    },
     methods: {
+        showLogin() {
 
-    }
+        },
+        check() {
+
+        }
+    },
+    components: {
+        Buttons
+    },
 }
--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        
-    </div>
+    <div class="login">
+        <div class="login-header">
+            <i @click="showLogin" class="fa fa-chevron-left" aria-hidden="true"></i>登录
+        </div>
+
+        <div class="login-input">
+            <input v-model="AccessToken" type="text" placeholder="请输入Access Token">
+        <span class="textBottom"></span>
+        <!-- <button @click.stop.prevent="check(inputVal)">登录</button>-->
+        <Buttons @click.stop.prevent="check(inputVal)" text="登录"></Buttons>
+        </div>
+
+        <div class="login-tips">
+            <ul>
+                <li>为了更好的体验请登录（づ￣3￣）づ╭❤～</li>
+                <li>在<a class="title" href="https://cnodejs.org">CNode官网</a>登录CNode账号</li>
+                <li>在<a class="title" href="https://cnodejs.org/setting">设置</a>中可以获取CNode账号的Access Token</li>
+                <li><img src="../../assets/img/Access Token.jpg" alt="如何获取CNode账号的Access Token"></li>
+            </ul>
+        </div>
+  </div>
 </template>
 
 <script>
