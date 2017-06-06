2dfe80f9a650205f405d54dc4827089e12d2aec5
LDQ-first <178848890@qq.com>
2017-06-06 14:10:49 +0800

    optimize about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,19 +8,20 @@ export default {
              {name: 'Vue2.2', des: '前端框架'},
              {name: 'Vuex', des: '状态管理,组件通信'}, 
              {name: 'Vue-router', des: '配置路由，组件切换'},
-             {name: 'axios', des: '基于Promise处理HTTP请求'},
+             {name: 'ES6/7', des: 'JS版本'},
+             {name: 'Webpack', des: '模块化处理，编译打包'},
+             {name: 'Express', des: '服务器'},
+             {name: 'Axios', des: '基于Promise处理HTTP请求'},
              {name: 'SASS', des: 'css预处理器'},
              {name: 'Normalize.css', des: '处理浏览器默认样式'},
              {name: 'Font-awesome', des: '字体图标'},
              {name: 'Moment.js', des: '处理时间显示'},
              {name: 'Simplemde', des: 'markdown编辑器'},
              {name: 'Highlight.js', des: '语法高亮'},
-             {name: 'ES6/7', des: 'JS版本'},
-             {name: 'Webpack', des: '模块化处理，编译打包'},
-             {name: 'Express', des: '服务器'},
              {name: 'Flex', des: '弹性布局'},
              {name: 'loaclStorge', des: '本地存储用户信息'}
             ],
+            skillIndex: 0,
         }
     },
     computed: {
@@ -35,5 +36,8 @@ export default {
         hiddenAbout() {
             this.$router.go(-1);
         },
+        skillClick(idx) {
+            this.skillIndex = idx;
+        }
     }
 }
--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -4,17 +4,22 @@
             <i @click="hiddenAbout" class="fa fa-chevron-left" aria-hidden="true"></i>关于
         </div>
       <div class="about-content">
-        <h2 class="about-content-title">Vue.js打造一个开源的CNode社区</h2>
+        <h2 class="about-content-title">Vue全家桶开发开源的CNode社区</h2>
         <p class="skill-title"><b>技术栈</b>：</p>
-        <ul class="skills">
-            <li v-for="(skill, index) in skills" :key="index" class="skill">
+        <ul class="skills" :style="{borderLeftColor: skinColor.replace(/\sl[\S\s]+/, '').replace(/(1)(\))/, '0.4$2')}">
+            <li v-for="(skill, index) in skills" :key="index" class="skill" :class="{active: index === skillIndex}" @click="skillClick(index)"
+            :style="{background: index === skillIndex ? skinColor.replace(/\sl[\S\s]+/, '').replace(/(1)(\))/, '0.4$2'): '',
+                    borderRightColor: skinColor.replace(/\sl[\S\s]+/, '')}">
                 <span class="name">{{skill.name}}</span>
-                <span class="des" v-show="skill.des">{{[skill.des]}}</span>
+                <span class="des" v-show="skill.des">[ {{skill.des}} ]</span>
             </li>
         </ul>
+        <div class="project">
+             <p class="link"><b>项目地址: </b><a class="title" href="https://github.com/LDQ-first/vue-CNode-1">GitHub</a></p>
+             <p class="link"><b>反馈: </b><a class="title" href="https://github.com/LDQ-first/vue-CNode-1/issues">Issues</a></p>
+             <p class="state"><b>声明: </b>本项目只供学习使用，请不要用作任何商业用途。</p>
+        </div>
       </div>
-       
-         
     </div>
 </template>
 
