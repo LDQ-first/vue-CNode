24e7879427f40de329dd4240d7acf43ee895c8c5
LDQ-first <178848890@qq.com>
2017-06-06 13:26:05 +0800

    optimize about component

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -4,13 +4,14 @@
             <i @click="hiddenAbout" class="fa fa-chevron-left" aria-hidden="true"></i>关于
         </div>
       <div class="about-content">
-        <h2>Vue.js打造一个开源的CNode社区</h2>
-        <p class="skill"><b>技术栈</b>：</p>
-        <div>
-            <ul>
-                
-            </ul>
-        </div>
+        <h2 class="about-content-title">Vue.js打造一个开源的CNode社区</h2>
+        <p class="skill-title"><b>技术栈</b>：</p>
+        <ul class="skills">
+            <li v-for="(skill, index) in skills" :key="index" class="skill">
+                <span class="name">{{skill.name}}</span>
+                <span class="des" v-show="skill.des">{{[skill.des]}}</span>
+            </li>
+        </ul>
       </div>
        
          
