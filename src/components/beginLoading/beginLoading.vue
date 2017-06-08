40a875606dfa8a06eaf98b1530860c798c3e5962
LDQ-first <178848890@qq.com>
2017-06-09 00:10:06 +0800

    optimize beginLoading component

--- src/components/beginLoading/beginLoading.vue
+++ src/components/beginLoading/beginLoading.vue
@@ -2,7 +2,6 @@
     <div id="beginLoading">
         <div class="loading">
              <div class="logoImg">
-                <!--<img src="../../assets/logo.png">-->
                 <img src="../../../static/img/cnodejs_light.svg">
             </div>
             <canvas id="canvas" width="200" height="200"  ref="canvasLoading"></canvas>
