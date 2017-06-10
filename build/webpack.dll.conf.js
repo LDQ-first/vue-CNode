9b24123c725c1cb2550891f27a788626b1f86434
LDQ-first <178848890@qq.com>
2017-06-10 11:06:19 +0800

    optimize router

---
+++ build/webpack.dll.conf.js
@@ -0,0 +1,8 @@
+/*const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        vendor: ['vue/dist/common.js', 'vue-router', 'babel-polyfill', 'axios']
+    }
+}*/
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ArticleList from '../components/ArticleList/ArticleList.vue'
-import Article from '../components/Article/Article.vue'
+/*import ArticleList from '../components/ArticleList/ArticleList.vue'*/
+/*import Article from '../components/Article/Article.vue'
 import User from '../components/User/User.vue'
 import Login from '../components/Login/Login.vue'
 import NewArticle from '../components/NewArticle/NewArticle.vue'
 import Msg from '../components/Msg/Msg.vue'
-import About from '../components/About/About.vue'
+import About from '../components/About/About.vue'*/
+const ArticleList = resolve => require(['../components/ArticleList/ArticleList.vue'], resolve)
+const Article = resolve => require(['../components/Article/Article.vue'], resolve)
+const User = resolve => require(['../components/User/User.vue'], resolve)
+const Login = resolve => require(['../components/Login/Login.vue'], resolve)
+const NewArticle = resolve => require(['../components/NewArticle/NewArticle.vue'], resolve)
+const Msg = resolve => require(['../components/Msg/Msg.vue'], resolve)
+const About = resolve => require(['../components/About/About.vue'], resolve)
 
 Vue.use(Router)
 
